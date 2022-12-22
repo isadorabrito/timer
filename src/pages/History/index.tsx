56639eb036f6e93a89057699e9c85c0925a5c102
Isadora Brito <isadoradpbrito@gmail.com>
2022-12-21 22:52:05 -0300

    initial commit

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,31 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>30 minutos</td>
+              <td>Há 3 meses</td>
+              <td>
+                <Status statusColor="green"> Concluído </Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
