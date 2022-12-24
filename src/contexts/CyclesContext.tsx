3dafbc584cb01733706a5d21f0d3eb4de0914ffb
Isadora Brito <isadoradpbrito@gmail.com>
2022-12-24 19:40:19 -0300

    refactor: fix reset function

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -69,7 +69,7 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
     setAmountSecondsPassed(0);
-    //reset();
+
   }
   function interruptCurrentCycle() {
     setCycles((state) =>
